d8d5c7804bc99cb1c7b84abc99b3c361bc43be98
Christoph Fuchs <dergigi@gmail.com>
2017-10-20 21:26:31 +0200

    Fix spinner init lifecycle

--- app/src/main/java/ml/homeo/homeo/MainActivity.java
+++ app/src/main/java/ml/homeo/homeo/MainActivity.java
@@ -5,20 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import static ml.homeo.homeo.R.array.dilutions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override/**/
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dilutions, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears/**/
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, dilutions, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
